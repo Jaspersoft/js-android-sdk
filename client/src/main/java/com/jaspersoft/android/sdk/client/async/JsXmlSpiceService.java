518b1f24c03c48cb37d2e91c046ef60fb701261f
Andrew Tivodar <andrewtivodar@yahoo.com>
2014-12-03 16:10:20 +0200

    Add InFileStringObjectPersister to the JsXmlSpiceService

--- client/src/main/java/com/jaspersoft/android/sdk/client/async/JsXmlSpiceService.java
+++ client/src/main/java/com/jaspersoft/android/sdk/client/async/JsXmlSpiceService.java
@@ -30,6 +30,7 @@ import com.octo.android.robospice.SpiceService;
 import com.octo.android.robospice.persistence.CacheManager;
 import com.octo.android.robospice.persistence.exception.CacheCreationException;
 import com.octo.android.robospice.persistence.springandroid.xml.SimpleSerializerObjectPersisterFactory;
+import com.octo.android.robospice.persistence.string.InFileStringObjectPersister;
 
 /**
  * This class offers a {@link SpiceService} dedicated to xml web services. Provides
@@ -44,6 +45,7 @@ public class JsXmlSpiceService extends SpiceService {
     public CacheManager createCacheManager(Application application) throws CacheCreationException {
         CacheManager cacheManager = new CacheManager();
         cacheManager.addPersister(new SimpleSerializerObjectPersisterFactory(application));
+        cacheManager.addPersister(new InFileStringObjectPersister(application));
         return cacheManager;
     }
 
