18363bf169fd35623c3a0835c38c1863b9911ce1
Tom Koptel <tom.koptel@gmail.com>
2015-09-23 15:37:20 +0300

    Add missing headers and copyright

--- client-service/src/main/java/com/jaspersoft/android/sdk/service/repository/SearchStrategy.java
+++ client-service/src/main/java/com/jaspersoft/android/sdk/service/repository/SearchStrategy.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright © 2015 TIBCO Software, Inc. All rights reserved.
+ * http://community.jaspersoft.com/project/jaspermobile-android
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of Jaspersoft Mobile for Android.
+ *
+ * Jaspersoft Mobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jaspersoft Mobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Jaspersoft Mobile for Android. If not, see
+ * <http://www.gnu.org/licenses/lgpl>.
+ */
+
 package com.jaspersoft.android.sdk.service.repository;
 
 import com.jaspersoft.android.sdk.network.api.RepositoryRestApi;
@@ -5,6 +29,10 @@ import com.jaspersoft.android.sdk.service.data.server.ServerVersion;
 
 import rx.Observable;
 
+/**
+ * @author Tom Koptel
+ * @since 2.0
+ */
 public interface SearchStrategy {
     Observable<SearchResult> search();
 
