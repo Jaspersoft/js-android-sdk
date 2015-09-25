fd16105974c026542362d36534161fc70beaacc9
Tom Koptel <tom.koptel@gmail.com>
2015-09-25 17:30:42 +0300

    Remove deprecated EmeraldMR2SearchStrategy

--- client-service/src/main/java/com/jaspersoft/android/sdk/service/repository/EmeraldMR2SearchStrategy.java
+++ client-service/src/main/java/com/jaspersoft/android/sdk/service/repository/EmeraldMR2SearchStrategy.java
@@ -24,8 +24,6 @@
 
 package com.jaspersoft.android.sdk.service.repository;
 
-import android.support.annotation.NonNull;
-
 import com.jaspersoft.android.sdk.network.api.RepositoryRestApi;
 import com.jaspersoft.android.sdk.network.entity.resource.ResourceLookupResponse;
 import com.jaspersoft.android.sdk.network.entity.resource.ResourceSearchResponse;
@@ -44,24 +42,19 @@ import rx.functions.Func0;
  */
 final class EmeraldMR2SearchStrategy implements SearchStrategy {
     private static final Collection<ResourceLookupResponse> EMPTY_RESPONSE = Collections.emptyList();
-    private static final int RETRY_COUNT = 5;
-    private final static int UNDEFINED = -1;
+    private static final int MAX_RETRY_COUNT = 5;
 
     private final RepositoryRestApi.Factory mRepoFactory;
     private final SearchCriteria mInitialCriteria;
 
-    private int mUserOffset;
-    private int mInternalOffset = UNDEFINED;
+    private int mServerDisposition;
+    private List<ResourceLookupResponse> mBuffer = new LinkedList<>();
     private boolean mEndReached;
-
-    private Collection<ResourceLookupResponse> tempBuffer = Collections.emptyList();
-    private SearchCriteria mNextCriteria;
 
     public EmeraldMR2SearchStrategy(RepositoryRestApi.Factory repositoryApiFactory, SearchCriteria criteria) {
         mRepoFactory = repositoryApiFactory;
         mInitialCriteria = criteria.newBuilder().create();
-
-        mUserOffset = criteria.getOffset();
+        mEndReached = false;
     }
 
     @Override
@@ -69,16 +62,15 @@ final class EmeraldMR2SearchStrategy implements SearchStrategy {
         return Observable.defer(new Func0<Observable<Collection<ResourceLookupResponse>>>() {
             @Override
             public Observable<Collection<ResourceLookupResponse>> call() {
-                if (mInternalOffset == UNDEFINED) {
-                    defineInternalOffset();
-                }
-                if (mEndReached){
+                int limit = mInitialCriteria.getLimit();
+                int offset = mInitialCriteria.getOffset();
+
+                if (mEndReached || limit == 0) {
                     return Observable.just(EMPTY_RESPONSE);
                 }
-                mNextCriteria = mInitialCriteria.newBuilder()
-                        .offset(mInternalOffset)
-                        .create();
-                return Observable.just(performAlignedRequests());
+
+                calculateDisposition(offset);
+                return Observable.just(internalSearch(limit));
             }
         });
     }
@@ -88,100 +80,46 @@ final class EmeraldMR2SearchStrategy implements SearchStrategy {
         return !mEndReached;
     }
 
-    private void defineInternalOffset() {
-        mInternalOffset = 0;
-        if (mUserOffset != 0) {
-            mNextCriteria = mInitialCriteria.newBuilder()
-                    .limit(mUserOffset)
-                    .offset(0)
-                    .create();
-            performAlignedRequests();
+    private void calculateDisposition(int offset) {
+        boolean serverDispositionUndefined = offset >= mServerDisposition;
+        if (serverDispositionUndefined) {
+            internalSearch(offset);
         }
     }
 
-    private Collection<ResourceLookupResponse> performAlignedRequests() {
-        int emptyRequestCount = 1;
-        ResourceSearchResponse response = performApiCall();
+    private Collection<ResourceLookupResponse> internalSearch(int limit) {
+        int count = 0;
+        while (mBuffer.size() < limit && hasNext()) {
+            ResourceSearchResponse response = performSearch(limit);
+            mBuffer.addAll(response.getResources());
+            mServerDisposition += limit;
 
-        List<ResourceLookupResponse> collectionFromApi = response.getResources();
-
-        List<ResourceLookupResponse> localBuffer = new LinkedList<>();
-        localBuffer.addAll(collectionFromApi);
-
-        Collection<ResourceLookupResponse> result;
-
-        if (tempBuffer.isEmpty()) {
-            result = alignBuffer(localBuffer, emptyRequestCount);
-        } else {
-            List<ResourceLookupResponse> accumulatedBuffer = new LinkedList<>(tempBuffer);
-            accumulatedBuffer.addAll(localBuffer);
-            result = alignBuffer(accumulatedBuffer, emptyRequestCount);
+            if (response.getResources().isEmpty()) {
+                /**
+                 * This is ugly condition. API for 5.5 is broken and there is no way to
+                 * determine weather we reached total count value.
+                 *
+                 * Corresponding situation happens if API has returned 204 for request.
+                 * We are trying to retry request 5 times. If 5 times sequentially
+                 * received 204 we consider that API has no more items for client to consume
+                 */
+                mEndReached = (count == MAX_RETRY_COUNT);
+                count++;
+            }
         }
 
+        int measure = Math.min(limit, mBuffer.size());
+        Collection<ResourceLookupResponse> result = mBuffer.subList(0, measure);
+        mBuffer = mBuffer.subList(measure, mBuffer.size());
         return result;
     }
 
-    private Collection<ResourceLookupResponse> alignBuffer(List<ResourceLookupResponse> accumulatedBuffer, int count) {
-        int limit = mNextCriteria.getLimit();
-        if (accumulatedBuffer.size() > limit) {
-            return cacheRemainedBuffer(accumulatedBuffer);
-        } else if (accumulatedBuffer.size() < limit) {
-            return alignBufferWithResponse(accumulatedBuffer, count);
-        } else {
-            tempBuffer = Collections.emptyList();
-            return accumulatedBuffer;
-        }
-    }
-
-    @NonNull
-    private Collection<ResourceLookupResponse> cacheRemainedBuffer(List<ResourceLookupResponse> buffer) {
-        int limit = mNextCriteria.getLimit();
-        Collection<ResourceLookupResponse> leftPart = buffer.subList(0, limit - 1);
-        Collection<ResourceLookupResponse> rightPart = buffer.subList(limit - 1, buffer.size());
-        tempBuffer = rightPart;
-        return leftPart;
-    }
-
-    private Collection<ResourceLookupResponse> alignBufferWithResponse(List<ResourceLookupResponse> resources, int count) {
-        ResourceSearchResponse response = performApiCall();
-        List<ResourceLookupResponse> collectionFromApi = response.getResources();
-        resources.addAll(collectionFromApi);
-
-        /**
-         * This is ugly condition. API for 5.5 is broken and there is no way to
-         * determine weather we reached total count value.
-         *
-         * Corresponding situation happens if API has returned 204 for request.
-         * We are trying to retry request 5 times. If 5 times sequentially
-         * received 204 we consider that API has no more items for client to consume
-         */
-        mEndReached = (count == RETRY_COUNT);
-        if (mEndReached) {
-            return resources;
-        }
-
-        if (collectionFromApi.isEmpty()) {
-            count++;
-        }
-
-        return alignBuffer(resources, count);
-    }
-
-    private ResourceSearchResponse performApiCall() {
-        RepositoryRestApi api = mRepoFactory.get();
-        ResourceSearchResponse response = api.searchResources(mNextCriteria.toMap());
-        updateNextOffset();
-        updateNextCriteria();
-        return response;
-    }
-
-    private void updateNextOffset() {
-        mInternalOffset += mNextCriteria.getLimit();
-    }
-
-    private void updateNextCriteria() {
-        mNextCriteria = mNextCriteria.newBuilder()
-                .offset(mInternalOffset)
+    private ResourceSearchResponse performSearch(int limit) {
+        SearchCriteria nextCriteria = mInitialCriteria.newBuilder()
+                .offset(mServerDisposition)
+                .limit(limit)
                 .create();
+        RepositoryRestApi api = mRepoFactory.get();
+        return api.searchResources(nextCriteria.toMap());
     }
 }
