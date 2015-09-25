36bc0ca48ccbabd643f9baa770a7554be746f5e1
Tom Koptel <tom.koptel@gmail.com>
2015-09-25 10:39:33 +0300

    Add intial implementation of Emerald2 searc starategy

--- client-service/src/main/java/com/jaspersoft/android/sdk/service/repository/EmeraldMR2SearchStrategy.java
+++ client-service/src/main/java/com/jaspersoft/android/sdk/service/repository/EmeraldMR2SearchStrategy.java
@@ -26,22 +26,111 @@ package com.jaspersoft.android.sdk.service.repository;
 
 import com.jaspersoft.android.sdk.network.api.RepositoryRestApi;
 import com.jaspersoft.android.sdk.network.entity.resource.ResourceLookupResponse;
+import com.jaspersoft.android.sdk.network.entity.resource.ResourceSearchResponse;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 import rx.Observable;
+import rx.functions.Func0;
 
 /**
  * @author Tom Koptel
  * @since 2.0
  */
 final class EmeraldMR2SearchStrategy implements SearchStrategy {
-    public EmeraldMR2SearchStrategy(RepositoryRestApi.Factory repositoryApiFactory, SearchCriteria criteria) {
+    private final RepositoryRestApi.Factory mRepoFactory;
+    private final SearchCriteria mInitialCriteria;
+    private final int mLimit;
 
+    private int mNextOffset;
+    private boolean mFirstCall;
+
+    public EmeraldMR2SearchStrategy(RepositoryRestApi.Factory repositoryApiFactory, SearchCriteria criteria) {
+        mRepoFactory = repositoryApiFactory;
+        mInitialCriteria = criteria.newBuilder().create();
+
+        mNextOffset = criteria.getOffset();
+        mLimit = criteria.getLimit();
+
+        mFirstCall = true;
     }
 
     @Override
     public Observable<Collection<ResourceLookupResponse>> search() {
-        return null;
+        return Observable.defer(new Func0<Observable<Collection<ResourceLookupResponse>>>() {
+            @Override
+            public Observable<Collection<ResourceLookupResponse>> call() {
+                return Observable.just(performAlignedRequests());
+            }
+        });
+    }
+
+    private Collection<ResourceLookupResponse> performAlignedRequests() {
+        ResourceSearchResponse response = makeCall();
+
+        List<ResourceLookupResponse> collectionFromApi = response.getResources();
+        if (collectionFromApi.isEmpty()) {
+            return collectionFromApi;
+        }
+
+        Collection<ResourceLookupResponse> buffer = new LinkedList<>();
+        buffer.addAll(collectionFromApi);
+
+        if (mLimit == 0) {
+            return buffer;
+        } else {
+            if (buffer.size() == mLimit) {
+                return buffer;
+            } else {
+                return alignResponse(buffer);
+            }
+        }
+    }
+
+    private Collection<ResourceLookupResponse> alignResponse(Collection<ResourceLookupResponse> buffer) {
+        ResourceSearchResponse response = makeCall();
+        List<ResourceLookupResponse> collectionFromApi = response.getResources();
+        if (collectionFromApi.isEmpty()) {
+            return buffer;
+        }
+
+        buffer.addAll(collectionFromApi);
+        int actual = buffer.size();
+
+        if (actual < mLimit) {
+            return alignResponse(buffer);
+        }
+        if (actual > mLimit) {
+            List<ResourceLookupResponse> resources = new ArrayList<>(buffer);
+            Collection<ResourceLookupResponse> aligned = resources.subList(0, mLimit - 1);
+            mNextOffset -= (actual - mLimit);
+            return aligned;
+        }
+        return buffer;
+    }
+
+    private ResourceSearchResponse makeCall() {
+        SearchCriteria newSearchCriteria = resolveNextCriteria();
+        RepositoryRestApi api = mRepoFactory.get();
+        return api.searchResources(newSearchCriteria.toMap());
+    }
+
+    private void updateNextOffset() {
+        if (!mFirstCall) {
+            mNextOffset += mLimit;
+        }
+        mFirstCall = false;
+    }
+
+    private SearchCriteria resolveNextCriteria() {
+        updateNextOffset();
+
+        SearchCriteria.Builder newCriteriaBuilder = mInitialCriteria.newBuilder();
+        newCriteriaBuilder.offset(mNextOffset);
+
+        return newCriteriaBuilder.create();
     }
 }
