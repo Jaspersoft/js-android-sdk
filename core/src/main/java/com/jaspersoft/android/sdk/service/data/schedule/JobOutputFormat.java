a89a1aa46c7085c9d93304e518c0492b9f98f9c2
andrewtivodar <andrewtivodar@yahoo.com>
2016-02-16 11:03:55 +0200

    Added PPTX format for JobOutputFormat

--- core/src/main/java/com/jaspersoft/android/sdk/service/data/schedule/JobOutputFormat.java
+++ core/src/main/java/com/jaspersoft/android/sdk/service/data/schedule/JobOutputFormat.java
@@ -5,5 +5,5 @@ package com.jaspersoft.android.sdk.service.data.schedule;
  * @since 2.0
  */
 public enum JobOutputFormat {
-    PDF, HTML, XLS, RTF, CSV, ODT, TXT, DOCX, ODS, XLSX, XLS_NOPAG, XLSX_NOPAG, DATA_SNAPSHOT,
+    PDF, HTML, XLS, RTF, CSV, ODT, TXT, DOCX, ODS, XLSX, XLS_NOPAG, XLSX_NOPAG, DATA_SNAPSHOT, PPTX
 }
