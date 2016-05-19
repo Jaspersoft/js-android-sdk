ee501216ed520e3bfd15c61679e5ef60182c2e57
Tom Koptel <tom.koptel@gmail.com>
2016-05-19 15:16:27 +0300

    Add javadocs for JobSearchCriteria

--- core/src/main/java/com/jaspersoft/android/sdk/service/report/schedule/JobSearchCriteria.java
+++ core/src/main/java/com/jaspersoft/android/sdk/service/report/schedule/JobSearchCriteria.java
@@ -7,12 +7,39 @@ import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.TestOnly;
 
 /**
+ * Configuration POJO that simplifies search task related operations.
+ * 
+ * <pre>
+ * {@code
+ *
+ *  JobSearchCriteria criteria = JobSearchCriteria.builder()
+ *          .withLabel("My qury")
+ *          .withOffset(10)
+ *          .withLimit(100)
+ *          .withReportUri("/report/uri")
+ *          .withAscending(true)
+ *          .withOwner(JobOwner.newOwner("user:oranization"))
+ *          .withSortType(JobSortType.SORTBY_JOBID)
+ *          .build();
+ * }
+ * </pre>
+ * 
  * @author Tom Koptel
  * @since 2.3
  */
 public final class JobSearchCriteria {
     public static int UNLIMITED_ROW_NUMBER = Integer.MAX_VALUE;
-
+    static {
+        JobSearchCriteria criteria = JobSearchCriteria.builder()
+                .withLabel("My qury")
+                .withOffset(10)
+                .withLimit(100)
+                .withReportUri("/report/uri")
+                .withAscending(true)
+                .withOwner(JobOwner.newOwner("user:oranization"))
+                .withSortType(JobSortType.SORTBY_JOBID)
+                .build();
+    }
     @Nullable
     private final String mReportUri;
     @Nullable
@@ -153,38 +180,81 @@ public final class JobSearchCriteria {
             return new JobSearchCriteria(this);
         }
 
+        /**
+         * URI of report to schedule. Report options URI can also be used here. Search term is case insensitive and may match any part of property.
+         *
+         * @param reportUri unique identifier of associated report
+         * @return builder for convenient configuration 
+         */
         public Builder withReportUri(@Nullable String reportUri) {
             mReportUri = ifEmptySubstituteStringWithNull(reportUri);
             return this;
         }
 
+        /**
+         * Name|Organization of the user, who scheduled the job
+         *
+         * @param owner specific owner of task
+         * @return builder for convenient configuration 
+         */
         public Builder withOwner(@Nullable JobOwner owner) {
             mOwner = owner;
             return this;
         }
 
+        /**
+         * Label of the jobs to find. Search term is case insensitive and may match any part of property.
+         * Full support of corresponding property was add in JRS 6.2.
+         *
+         * @param label or query used to search against API.
+         * @return builder for convenient configuration 
+         */
         public Builder withLabel(@Nullable String label) {
             mLabel = ifEmptySubstituteStringWithNull(label);
             return this;
         }
 
+        /**
+         * Pagination, start index
+         *
+         * @param startIndex offset of the particular search
+         * @return builder for convenient configuration 
+         */
         public Builder withOffset(int startIndex) {
             Preconditions.checkArgument(startIndex >= 0, "Start index must be positive");
             mOffset = startIndex;
             return this;
         }
 
+        /**
+         * Pagination, results count in a page
+         *
+         * @param limit the numberOfRows returned by API
+         * @return builder for convenient configuration 
+         */
         public Builder withLimit(int limit) {
             Preconditions.checkArgument(limit > 0, "Row number must be positive");
             mLimit = limit;
             return this;
         }
 
+        /**
+         * Field name to sort by.
+         *
+         * @param jobSortType can be any value from {@link JobSortType}
+         * @return builder for convenient configuration 
+         */
         public Builder withSortType(@Nullable JobSortType jobSortType) {
             mJobSortType = jobSortType;
             return this;
         }
 
+        /**
+         * Sorting direction.
+         * 
+         * @param ascending Supported values: true - ascending; false - descending
+         * @return builder for convenient configuration 
+         */
         public Builder withAscending(@Nullable Boolean ascending) {
             mAscending = ascending;
             return this;
