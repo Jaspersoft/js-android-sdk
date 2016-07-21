db540275716a8da1c0341136d0429c9f046f92ab
andrewtivodar <andrewtivodar@yahoo.com>
2016-07-21 12:54:30 +0300

    Make bookmark parcelable

--- widget/src/main/java/com/jaspersoft/android/sdk/widget/report/renderer/Bookmark.java
+++ widget/src/main/java/com/jaspersoft/android/sdk/widget/report/renderer/Bookmark.java
@@ -1,12 +1,15 @@
 package com.jaspersoft.android.sdk.widget.report.renderer;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
 /**
  * @author Andrew Tivodar
  * @since 2.6
  */
-public class Bookmark {
+public class Bookmark implements Parcelable{
     private final String anchor;
     private final int page;
     private final ArrayList<Bookmark> bookmarks;
@@ -15,6 +18,12 @@ public class Bookmark {
         this.anchor = anchor;
         this.page = page;
         this.bookmarks = bookmarks;
+    }
+
+    private Bookmark(Parcel in) {
+        anchor = in.readString();
+        page = in.readInt();
+        bookmarks = in.readArrayList(Bookmark.class.getClassLoader());
     }
 
     public String getAnchor() {
@@ -28,4 +37,28 @@ public class Bookmark {
     public ArrayList<Bookmark> getBookmarks() {
         return bookmarks;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(anchor);
+        dest.writeInt(page);
+        dest.writeTypedList(bookmarks);
+    }
+
+    public static final Parcelable.Creator<Bookmark> CREATOR = new Parcelable.Creator<Bookmark>() {
+        @Override
+        public Bookmark createFromParcel(Parcel in) {
+            return new Bookmark(in);
+        }
+
+        @Override
+        public Bookmark[] newArray(int size) {
+            return new Bookmark[size];
+        }
+    };
 }
