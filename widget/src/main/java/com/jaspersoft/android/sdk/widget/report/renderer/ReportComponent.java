0938b06157571f20156f257098730cdd16ffba07
Aleksandr Dakhno <odahno@tibco.com>
2016-08-19 09:15:04 +0300

    Extend 'ReportComponent' - handle chart type.

--- widget/src/main/java/com/jaspersoft/android/sdk/widget/report/renderer/ReportComponent.java
+++ widget/src/main/java/com/jaspersoft/android/sdk/widget/report/renderer/ReportComponent.java
@@ -8,6 +8,9 @@ package com.jaspersoft.android.sdk.widget.report.renderer;
 public class ReportComponent {
     private String id;
     private String componentType;
+    // TODO: move into child class (ChartReportComponent)
+    private ChartType mChartTypeInstance;
+    private String chartType;
 
     public String getId() {
         return id;
@@ -23,5 +26,26 @@ public class ReportComponent {
 
     public void setComponentType(String componentType) {
         this.componentType = componentType;
+    }
+
+    public ChartType getChartTypeInstance() {
+        if (componentType.equals("chart")) {
+            mChartTypeInstance = new ChartType(chartType);
+        } else {
+            // TODO: implement other cases
+        }
+        return mChartTypeInstance;
+    }
+
+    public void setChartTypeInstance(ChartType chartTypeInstance) {
+        this.mChartTypeInstance = chartTypeInstance;
+    }
+
+    public String getChartType() {
+        return chartType;
+    }
+
+    public void setChartType(String chartType) {
+        this.chartType = chartType;
     }
 }
